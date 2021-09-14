ce2ce6d3dbab8e2e5b54f16929558c1922be4857
SalauddinAyub <ayub35-265@diu.edu.bd>
2021-09-14 09:50:14 +0600

    Categories

--- firstblog/urls.py
+++ firstblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
    path('', HomeView.as_view(), name = "home"),
@@ -9,4 +9,5 @@ urlpatterns = [
    path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
    path('add_category/', AddCategoryView.as_view(), name="add_category"),
    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
 ]
